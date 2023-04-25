300e729c4639fdae00ee80ecbff55aa6347bf9c8
Rodrigo Klaes <rodrigoklaes@gmail.com>
2023-04-25 01:28:42 -0300

    created job domain and tests

--- domain/job.go
+++ domain/job.go
@@ -1,8 +1,52 @@
 package domain
 
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
 type Job struct {
-	ID string
-	OutputBucketPath string
-	Status string
-	Video *Video
+	ID string `valid:"uuid"`
+	OutputBucketPath string `valid:"notnull"`
+	Status string `valid:"notnull"`
+	Video *Video `valid:"-"`
+	VideoID string `valid:"-"`
+	Error string `valid:"-"`
+	CreatedAt time.Time `valid:"-"`
+	UpdatedAt time.Time `valid:"-"`
+}
+
+func NewJob(output string, status string, video *Video) (*Job, error){
+	job := Job{
+		OutputBucketPath: output,
+		Status: status,
+		Video: video,
+	}
+
+	job.prepare()
+
+	err := job.Validate()
+
+	if(err != nil){
+		return nil, err
+	}
+
+	return &job, nil
+}
+
+func (job *Job) prepare(){
+	job.ID = uuid.NewV4().String()
+	job.CreatedAt = time.Now()
+	job.UpdatedAt = time.Now()
+} 
+
+func (job *Job) Validate() error {
+	_, err := govalidator.ValidateStruct(job)
+	if(err != nil){
+		return err
+	}
+
+	return nil
 }
